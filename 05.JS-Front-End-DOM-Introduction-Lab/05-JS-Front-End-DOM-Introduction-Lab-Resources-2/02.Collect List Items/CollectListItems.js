5a463a4d0d7dd8897718f33f336610ef443d49ba
Hristomir Vasilev <hristomirster@gmail.com>
2024-04-06 20:17:32 +0300

    a

--- 05.JS-Front-End-DOM-Introduction-Lab/05-JS-Front-End-DOM-Introduction-Lab-Resources-2/02.Collect List Items/CollectListItems.js
+++
@@ -1,9 +0,0 @@
-function extractText() {
-  let itemNodes = document.querySelectorAll("ul li");
-  let textarea = document.querySelector('#result');
-
-  console.log(textarea);
-  for (let node of itemNodes) {
-    textarea.value += node.textContent + "\n";
-  }
-}
