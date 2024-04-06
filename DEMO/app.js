bc82e2b6ab8cdff5d63e2b498941d2b1051895a2
Hristomir Vasilev <hristomirster@gmail.com>
2024-04-06 20:22:20 +0300

    u

--- DEMO/app.js
+++
@@ -1,6 +0,0 @@
-function solve(input) {
-    if(input < 1 && input > 12)
-    console.log('Error!');
-};
-
-solve(1111);
--- temp/app.js
+++
@@ -1,6 +0,0 @@
-const url = "https://swapi.dev/api";
-
-fetch(`${url}/people/`, {method: 'GET'})
-    .then((response) => (response.json()))
-    .then(data => console.log(data))
-    .catch(error => console.log("Somthing went wrong"));
